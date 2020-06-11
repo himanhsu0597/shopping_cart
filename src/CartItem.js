230d80ecef40025f6c3cd7028380103a7a2113cf
InvincibleMe <your_email@example.com>
2020-06-11 18:27:43 +0530

    Adding action buttons to CartItem Component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 class  CartItem extends React.Component{
-
+    constructor()
+    {
+        super();
+       this.state={
+            price:999,
+            qty:1,
+            title:"My Phone",
+            url:""
+       };
+    }
     render()
     {
+        const {price,qty,title}=this.state;
         return(
           <div className="cart-item">
 
@@ -13,11 +23,15 @@ class  CartItem extends React.Component{
             </div>
 
             <div className="right-block">
-                <div style={{fontSize:"25px"}}>Phone</div>
-                <div style={{color:"gray"}}>Rs 999</div>
-                <div >Qty: 10</div>
+                <div style={{fontSize:"25px"}}>{title}</div>
+                <div style={{color:"gray"}}>Rs {price}</div>
+                <div style={{color:"gray"}}>Qty: {qty}</div>
                 <div className="cart-item-actions" >
-                    
+                    {/*Buttons*/}
+                    <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" />
+                    <img alt="decrease"  className="action-icons" src="https://image.flaticon.com/icons/svg/659/659892.svg"/>
+                    <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
+
                 </div>
             </div>
           </div>
@@ -31,7 +45,7 @@ image:{
     height: 100,
   width: 100,
   borderRadius:4,
-  backgroundColor:"red"
+  backgroundColor:"Gray"
 }
 
 }
