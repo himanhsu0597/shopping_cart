6893022f3e1b92c0beece2249c29fe7b921b0b14
InvincibleMe <your_email@example.com>
2020-06-12 18:47:19 +0530

    Shifting states and functionalities to Cart component

--- src/Cart.js
+++ src/Cart.js
@@ -2,15 +2,102 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component{
+
+    constructor()
+    {
+        super();
+       this.state={
+           products:[
+                {
+                    price:999,
+                    qty:1,
+                    title:"Phone",
+                    url:"",
+                    id:1
+                },
+                {
+                    price:9990,
+                    qty:2,
+                    title:"Tablet",
+                    url:"",
+                    id:2
+                },
+                {
+                    price:99,
+                    qty:10,
+                    title:"Watch",
+                    url:"",
+                    id:3
+                },
+                {
+                    price:99900,
+                    qty:3,
+                    title:"Laptop",
+                    url:"",
+                    id:4
+                }
+
+           ]
+       }
+       //this.increaseQuantity=this.increaseQuantity.bind(this);
+      // this.testing();
+    }
+
+    handleIncreaseQuantity = (product) => {
+        console.log("output product",product);
+        const {products}=this.state;
+        const index=products.indexOf(product);
+        products[index].qty+=1;
+
+        this.setState({
+            products
+        })
+
+    }
+    handleDecreaseQuantity = (product) => {
+        console.log("output product",product);
+        const {products}=this.state;
+        const index=products.indexOf(product);
+        if( products[index].qty==0)
+        return;
+        products[index].qty-=1;
+
+        this.setState({
+            products
+        })
+
+    }
+    handleDeleteQuantity = (id) => {
+        console.log("output product",id);
+        const {products}=this.state;
+        const newProducts=products.filter((product)=>product.id!==id);
+        
+
+        this.setState({
+            products:newProducts
+        })
+
+    }
     render()
     {
+       // console.log(this.state);
+        const { products }=this.state;
         return (
             <div className="cart">
-                <CartItem></CartItem>
-                <CartItem></CartItem>
-                <CartItem></CartItem>
-                <CartItem></CartItem>
-
+            {
+                products.map((product)=>{
+                    return (
+                        <CartItem 
+                                product={product}
+                                key={product.id}
+                                increaseQuantity={this.handleIncreaseQuantity}
+                                decreaseQuantity={this.handleDecreaseQuantity}
+                                deleteQuantity={this.handleDeleteQuantity}
+                        />
+                    )
+                    
+                })
+            }
             </div>
         )
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -3,75 +3,10 @@ import { resolve } from 'url';
 import { reject } from 'q';
 
 class  CartItem extends React.Component{
-    constructor()
-    {
-        super();
-       this.state={
-            price:999,
-            qty:1,
-            title:"My Phone",
-            url:""
-       };
-       //this.increaseQuantity=this.increaseQuantity.bind(this);
-      // this.testing();
-    }
-     increaseQuantity=()=>
-    {
-        //this.state.qty+=1;   doesn't re render ,though the value do gets changed
-       // console.log('this.state', this.state);
-        // type 1 change shallow merge.Use this when new state is not dependent on previous state
-        // this.setState({
-        //     qty:this.state.qty+1
-        // });
-             // type 2 change shallow merge .Use this when new state is dependent on previous state
-        this.setState((prevState)=>{
-            return{
-            qty:prevState.qty+1
-            }
-        },()=>{
-            console.log(this.state);
-        });
-
-    }
-    decreaseQuantity=()=>{
-        const {qty}=this.state;
-        if(qty===0)
-        {
-           // console.log("Quantity can't ve negative");
-            return;
-        }
-    //    console.log('this.state', this.state);
-
-                this.setState((prevState)=>{
-                    return {
-                        qty:prevState.qty-1
-                    }
-                },()=>{
-                    console.log(this.state);
-                });
-    }
-    testing()
-    {
-        const promise=new Promise((resolve,reject)=>{
-            setTimeout(()=>{
-                resolve('done');
-
-            },5000);
-
-        });
-        promise.then(()=>{
-
-            this.setState({qty:this.state.qty+10});
-            this.setState({qty:this.state.qty+10});
-            this.setState({qty:this.state.qty+10});
-            //updated 100 will be visible because setState acts like synchronous function in Promise 
-            console.log(this.state);
-        })
-    }
-
     render()
     {
-        const {price,qty,title}=this.state;
+        console.log("this.props",this.props);
+        const {price,qty,title,id}=this.props.product;
         return(
           <div className="cart-item">
 
@@ -90,18 +25,20 @@ class  CartItem extends React.Component{
                             alt="increase"
                             className="action-icons"
                             src="https://image.flaticon.com/icons/svg/992/992651.svg"
-                            onClick={this.increaseQuantity}
+                            onClick={()=>this.props.increaseQuantity(this.props.product)}
                         />
                     <img
                             alt="decrease"
                             className="action-icons"
                             src="https://image.flaticon.com/icons/svg/659/659892.svg"
-                            onClick={this.decreaseQuantity}
+                            onClick={()=>this.props.decreaseQuantity(this.props.product)}
                         />
                     <img
                             alt="delete"
                             className="action-icons"
                             src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+                            onClick={()=>this.props.deleteQuantity(this.props.product.id)}
+                                
                         />
 
                 </div>
@@ -109,6 +46,61 @@ class  CartItem extends React.Component{
           </div>
         );
     }
+    //  increaseQuantity=()=>
+    // {
+    //     //this.state.qty+=1;   doesn't re render ,though the value do gets changed
+    //    // console.log('this.state', this.state);
+    //     // type 1 change shallow merge.Use this when new state is not dependent on previous state
+    //     // this.setState({
+    //     //     qty:this.state.qty+1
+    //     // });
+    //          // type 2 change shallow merge .Use this when new state is dependent on previous state
+    //     this.setState((prevState)=>{
+    //         return{
+    //         qty:prevState.qty+1
+    //         }
+    //     },()=>{
+    //         console.log(this.state);
+    //     });
+
+    // }
+    // decreaseQuantity=()=>{
+    //     const {qty}=this.state;
+    //     if(qty===0)
+    //     {
+    //        // console.log("Quantity can't ve negative");
+    //         return;
+    //     }
+    // //    console.log('this.state', this.state);
+
+    //             this.setState((prevState)=>{
+    //                 return {
+    //                     qty:prevState.qty-1
+    //                 }
+    //             },()=>{
+    //                 console.log(this.state);
+    //             });
+    // }
+    // testing()
+    // {
+    //     const promise=new Promise((resolve,reject)=>{
+    //         setTimeout(()=>{
+    //             resolve('done');
+
+    //         },5000);
+
+    //     });
+    //     promise.then(()=>{
+
+    //         this.setState({qty:this.state.qty+10});
+    //         this.setState({qty:this.state.qty+10});
+    //         this.setState({qty:this.state.qty+10});
+    //         //updated 100 will be visible because setState acts like synchronous function in Promise 
+    //         console.log(this.state);
+    //     })
+    // }
+
+   
 
 }
 
